7010c64721b35c1fba3ba134c7d867d2d152306d
Lactobacillus <cyw314@gmail.com>
2017-01-06 00:01:44 +0900

    we can see traffic

--- server.py
+++ server.py
@@ -2,14 +2,14 @@ import os
 import pickle
 from flask import Flask, request, render_template, redirect
 
-count = 0
+count = [0]
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
 @app.route('/index', methods = ['GET'])
 def index():
 
-	count = count + 1
+	count[0] += 1
 
 	if request.method == 'GET':
 
@@ -24,37 +24,38 @@ def index():
 @app.route('/draw', methods = ['GET'])
 def draw():
 
-	count = count + 1
+	count[0] += 1
 
 	return render_template('draw.html')
 
 @app.route('/map', methods = ['GET'])
 def map():
 
-	count = count + 1
+	count[0] += 1
 
 	return render_template('map.html')
 
 @app.route('/show', methods = ['GET', 'POST'])
 def show():
 
-	count = count + 1
+	count[0] += 1
 
 	return render_template('show.html')
 
 @app.route('/about', methods = ['GET'])
 def about():
 
-	count = count + 1
+	count[0] += 1
 
 	return render_template('about.html')
 
 @app.route('/traffic', methods = ['GET'])
 def traffic():
 
-	return count
+	return str(count[0])
 
 if __name__ == '__main__':
 
+	count = [0]
 	port = int(os.environ.get('PORT', 5000))
 	app.run(host = '0.0.0.0', port = port, debug = True)
