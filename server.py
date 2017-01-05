e9b7d720857fb01e7ed74c934f130f37d72f993e
Lactobacillus <cyw314@gmail.com>
2017-01-05 23:58:48 +0900

    test commit

--- server.py
+++ server.py
@@ -2,11 +2,14 @@ import os
 import pickle
 from flask import Flask, request, render_template, redirect
 
+count = 0
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
 @app.route('/index', methods = ['GET'])
 def index():
+
+	count = count + 1
 
 	if request.method == 'GET':
 
@@ -21,22 +24,35 @@ def index():
 @app.route('/draw', methods = ['GET'])
 def draw():
 
+	count = count + 1
+
 	return render_template('draw.html')
 
 @app.route('/map', methods = ['GET'])
 def map():
+
+	count = count + 1
 
 	return render_template('map.html')
 
 @app.route('/show', methods = ['GET', 'POST'])
 def show():
 
+	count = count + 1
+
 	return render_template('show.html')
 
 @app.route('/about', methods = ['GET'])
 def about():
 
+	count = count + 1
+
 	return render_template('about.html')
+
+@app.route('/traffic', methods = ['GET'])
+def traffic():
+
+	return count
 
 if __name__ == '__main__':
 
