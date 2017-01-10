69382355f6f0e80534186247d116a1bba5ef4248
Lactobacillus <cyw314@gmail.com>
2017-01-11 01:40:57 +0900

    little test

--- server.py
+++ server.py
@@ -2,14 +2,15 @@ import os
 import pickle
 from flask import Flask, request, render_template, redirect
 
-count = [0]
+count = 0
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
 @app.route('/index', methods = ['GET'])
 def index():
 
-	count[0] += 1
+	global count
+	count += 1
 
 	if request.method == 'GET':
 
@@ -24,21 +25,24 @@ def index():
 @app.route('/draw', methods = ['GET'])
 def draw():
 
-	count[0] += 1
+	global count
+	count += 1
 
 	return render_template('draw.html')
 
 @app.route('/map', methods = ['GET'])
 def map():
 
-	count[0] += 1
+	global count
+	count += 1
 
 	return render_template('map.html')
 
 @app.route('/show', methods = ['GET', 'POST'])
 def show():
 
-	count[0] += 1
+	global count
+	count += 1
 
 	if request.method == 'POST':
 		
@@ -64,7 +68,8 @@ def show():
 @app.route('/about', methods = ['GET'])
 def about():
 
-	count[0] += 1
+	global count
+	count += 1
 
 	return render_template('about.html')
 
@@ -80,7 +85,6 @@ def d3():
 
 if __name__ == '__main__':
 
-	count = [0]
 	#debug 모드 없애기#############################################
 	port = int(os.environ.get('PORT', 5000))
 	app.run(host = '0.0.0.0', port = port, debug = True)
