0e973a4950cebf7565ea830ab53198270f15a398
Lactobacillus <cyw314@gmail.com>
2017-01-11 01:34:18 +0900

    good parser is dead parser

--- parser.py
+++ parser.py
@@ -30,14 +30,79 @@ def get_htmls(fileName):
 	for url in urls:
 
 		response = urllib.request.urlopen(url)
-		html = response.read().decode('cp949')
-		html = BeautifulSoup(unicodedata.normalize('NFKD', html))
+		html = BeautifulSoup(response.read().decode('cp949'))
 		htmls.append(html)
-
-		print(html)
 
 	return htmls
 
+def get_time(text):
+
+	time = list()
+
+	for temp in text.split('\n'):
+
+		temp = temp.split(' ')[0]
+
+		if '-' in temp:
+
+			for i in range(int(temp[2]), int(temp[4]) + 1):
+
+				time.append(temp[0] + str(i))
+
+		else:
+
+			time.append(temp[0] + temp[2])
+
+	return time
+
+def get_room(text):
+
+	room = list()
+
+	for temp in text.split('\n'):
+
+		room.append(temp.split(')')[1][1:])
+
+	return list(set(room))
+
+def get_lectures(htmls):
+
+	lectures = list()
+
+	for html in htmls:
+
+		for row in html.find_all('tr'):
+
+			lecture = dict()
+			cols = row.find_all('td')
+
+			if len(cols) == 17 and len(cols[7].text) > 0:
+
+				try:
+
+					lecture['haksu'] = cols[1].text.replace('\n', '')
+					lecture['bunban'] = cols[2].text
+					lecture['name'] = unicodedata.normalize('NFKC', cols[4].text.replace('\t', '').replace('\n', '').replace('\r', ''))[:-1]
+					lecture['professor'] = cols[5].text
+					lecture['hakjum'] = cols[6].text[2]
+					lecture['time-room'] = cols[7].text
+					lecture['time'] = get_time(cols[7].text)
+					lecture['room'] = get_room(cols[7].text)
+
+					print(lecture)
+
+					lectures.append(lecture)
+				
+				except:
+
+					pass
+
+	return lectures
+
 if __name__ == '__main__':
 
-	print(get_htmls('urlList.txt'))
+	# run in linux
+	htmls = get_htmls('urlList.txt')
+	lectures = get_lectures(htmls)
+	save('2016-2.pickle', lectures)
+	print(len(lectures))
