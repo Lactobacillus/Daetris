7770c7e7a7f77d06d97adf18ee04f572d0a4f321
Lactobacillus <cyw314@gmail.com>
2017-01-20 03:07:14 +0900

    please work

--- visitor.py
+++ visitor.py
@@ -8,7 +8,7 @@ class Visitor(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	count = db.Column(db.Integer)
 
-	def __init__(self, visitor):
+	def __init__(self, count):
 
 		self.count = count
 
