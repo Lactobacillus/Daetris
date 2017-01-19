8d7436024fcfb81ed3baee39f5fb722e3c90831f
Lactobacillus <cyw314@gmail.com>
2017-01-19 14:52:40 +0900

    css 정리

--- server.py
+++ server.py
@@ -250,4 +250,4 @@ if __name__ == '__main__':
 
 	#debug 모드 없애기#############################################
 	port = int(os.environ.get('PORT', 5000))
-	app.run(host = '0.0.0.0', port = port)
+	app.run(host = '0.0.0.0', port = port, debug = True)
