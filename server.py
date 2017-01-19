cc6a27228d50fe0b41f51fe90b6871e6b4616ed2
Lactobacillus <cyw314@gmail.com>
2017-01-20 02:42:39 +0900

    bug fix

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL'] 
 db = SQLAlchemy(app)
+db.create_all()
 
 with open('2017-1.pickle', 'rb') as f:
 
